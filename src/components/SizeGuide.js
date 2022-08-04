f46479d2432c4d19b57180359b322baa99d83a5a
davidsudar <davidsudar25@gmail.com>
2022-08-05 00:21:41 +1000

    Updated sizing guide

--- src/components/SizeGuide.js
+++ src/components/SizeGuide.js
@@ -21,89 +21,88 @@ export default function SizeGuide() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="container mx-auto px-4 sm:px-8 max-w-3xl">
-      <div className="p-8">
-        <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 pt-4 overflow-x-auto">
-          <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
-            <table className="min-w-full leading-normal">
-              <thead>
-                <tr className=" bg-stone-200">
-                  <th
-                    scope="col"
-                    className="px-5 py-3  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                  ></th>
-                  <th
-                    scope="col"
-                    className="px-5 py-3 border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
-                  ></th>
-                  <th
-                    scope="col"
-                    colSpan="2"
-                    className="text-center px-5 py-3  border border-gray-200 text-gray-800 text-sm uppercase font-normal"
-                  >
-                    Chest
-                  </th>
+    <div className=" max-w-3xl my-4">
+      <div className="flex flex-col">
+        <div className="inline-block max-w-full shadow rounded-lg overflow-hidden">
+          <table className="max-w-full leading-normal">
+            <thead>
+              <tr className=" bg-stone-200">
+                <th
+                  scope="col"
+                  className="px-5 py-3  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                ></th>
+                <th
+                  scope="col"
+                  className="px-5 py-3 border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
+                ></th>
+                <th
+                  scope="col"
+                  colSpan="2"
+                  className="text-center px-5 py-3  border border-gray-200 text-gray-800 text-sm uppercase font-normal"
+                >
+                  Chest
+                </th>
+              </tr>
+              <tr className=" bg-stone-200">
+                <th
+                  scope="col"
+                  className="px-5 py-3   border-b border-gray-200 text-gray-800  text-center text-sm uppercase font-normal"
+                >
+                  Size
+                </th>
+                <th
+                  scope="col"
+                  className="px-5 py-3  border-b border-gray-200 text-gray-800  text-center text-sm uppercase font-normal"
+                >
+                  AUS
+                </th>
+                <th
+                  scope="col"
+                  className="px-5 py-3   border-b border-gray-200 text-gray-800  text-center text-sm uppercase font-normal"
+                >
+                  Inches
+                </th>
+                <th
+                  scope="col"
+                  className="px-5 py-3   border-b border-gray-200 text-gray-800  text-center text-sm uppercase font-normal"
+                >
+                  CM
+                </th>
+              </tr>
+            </thead>
+            <tbody className=" bg-stone-100">
+              {tshirt.map((item) => (
+                <tr className="text-center" key={item.Size + "-tshirt"}>
+                  <td className="px-5 py-5 border-b border-gray-200  text-sm bold">
+                    <p className="text-gray-900 whitespace-no-wrap">
+                      {item.Size}
+                    </p>
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap">
+                      {item.AusSize}
+                    </p>
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap">
+                      {item.ChestIn}
+                    </p>
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap">
+                      {item.ChestCm}
+                    </p>
+                  </td>
                 </tr>
-                <tr className=" bg-stone-200">
-                  <th
-                    scope="col"
-                    className="px-5 py-3   border-b border-gray-200 text-gray-800  text-center text-sm uppercase font-normal"
-                  >
-                    Size
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-5 py-3  border-b border-gray-200 text-gray-800  text-center text-sm uppercase font-normal"
-                  >
-                    AUS
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-5 py-3   border-b border-gray-200 text-gray-800  text-center text-sm uppercase font-normal"
-                  >
-                    Inches
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-5 py-3   border-b border-gray-200 text-gray-800  text-center text-sm uppercase font-normal"
-                  >
-                    CM
-                  </th>
-                </tr>
-              </thead>
-              <tbody className=" bg-stone-100">
-                {tshirt.map((item) => (
-                  <tr className="text-center" key={item.Size + "-tshirt"}>
-                    <td className="px-5 py-5 border-b border-gray-200  text-sm bold">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                        {item.Size}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                        {item.AusSize}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                        {item.ChestIn}
-                      </p>
-                    </td>
-                    <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                      <p className="text-gray-900 whitespace-no-wrap">
-                        {item.ChestCm}
-                      </p>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-          <button className="underline text-xs" onClick={() => setIsOpen(true)}>
-            How To Measure
-          </button>
+              ))}
+            </tbody>
+          </table>
         </div>
+        <button className="underline text-xs" onClick={() => setIsOpen(true)}>
+          How To Measure
+        </button>
       </div>
+
       <HowToMeasureModal isOpen={isOpen} setIsOpen={setIsOpen} />
     </div>
   );
--- src/pages/Sizing.js
+++ src/pages/Sizing.js
@@ -1,18 +1,13 @@
+import SideBar from "../components/Sidebar";
 import SizeGuide from "../components/SizeGuide";
 
 const Sizing = () => (
-  <div>
-    <h1 className="text-2xl font-semibold text-gray-800 uppercase dark:text-white lg:text-3xl sm:px-8 max-w-3xl lg:pt-8 sm:pt-4 md:pt-4" >
-      tshirt SIZE GUIDE
-    </h1>
+  <div className="flex justify-between">
+    <SideBar />
+    {/* <p className="uppercase text-3xl">tshirt SIZE GUIDE</p> */}
 
-    <SizeGuide guide="tshirt" />
-
-    <h1 className="text-2xl font-semibold text-gray-800 uppercase dark:text-white lg:text-3xl sm:px-8 max-w-3xl lg:pt-8">
-      leggings
-    </h1>
-
-    <SizeGuide guide="tshirt" />
+    <SizeGuide guide="tshirt"/>
+    <div></div>
   </div>
 );
 
