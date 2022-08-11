a682378ca20c66d2c6caab2624a404f84ff4c027
davidsudar <davidsudar25@gmail.com>
2022-08-12 01:32:51 +1000

    Updated FAQ Page

--- src/pages/FAQ.js
+++ src/pages/FAQ.js
@@ -1,12 +1,61 @@
-import FAQAccordian from "../components/FAQAccordian";
+import { NavLink } from "react-router-dom";
 
-const FAQ = () => (
-    <div className='flex flex-col justify-center'>
-      <h1 className="bg-blue-400 inline-block ml-5">FAQ</h1>
-      <FAQAccordian/>
-      <p>This is a test paragraph</p>
+const faqs = [
+  {
+    question: 'When will my order ship?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
+  },
+  {
+    question: 'I need to change something in my order. How do I do that?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
+  },
+  {
+    question: 'What is your returns policy?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
+  },
+  {
+    question: 'How long does an item take to ship?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
+  },
+  {
+    question: 'Is the clothing true to size?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.',
+  },
+  // More questions...
+]
+
+export default function FAQ() {
+  return (
+    <div className="bg-stone-300">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
+        <div className="lg:grid lg:grid-cols-3 lg:gap-8">
+          <div>
+            <h2 className="text-3xl font-extrabold text-gray-900">Frequently asked questions</h2>
+            <p className="mt-4 text-lg text-gray-500">
+              Can't find the answer you're looking for? Reach out to our{' '}
+              <NavLink to="/" className="font-medium text-indigo-600 hover:text-indigo-500">
+                customer support
+              </NavLink>{' '}
+              team.
+            </p>
+          </div>
+          <div className="mt-12 lg:mt-0 lg:col-span-2">
+            <dl className="space-y-12">
+              {faqs.map((faq) => (
+                <div key={faq.question}>
+                  <dt className="text-lg leading-6 font-medium text-gray-900">{faq.question}</dt>
+                  <dd className="mt-2 text-base text-gray-500">{faq.answer}</dd>
+                </div>
+              ))}
+            </dl>
+          </div>
+        </div>
       </div>
-
-  );
-
-  export default FAQ;
+    </div>
+  )
+}
